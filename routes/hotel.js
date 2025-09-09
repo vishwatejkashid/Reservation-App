681d40da94b80a86a2f609096df732c3f327cece
Vishwatej Kashid <vishwatejkashid18@gmail.com>
2025-09-09 22:34:22 +0530

    Integrated with front-end

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createHotel , updateHotel, deleteHotel, getHotel, getAllHotel  } from "../controllers/hotel.js"
+import {createHotel , updateHotel, deleteHotel, getHotel, getAllHotel, countByCity  } from "../controllers/hotel.js"
 import { validateAdmin } from "../utils/validation.js";
 const router = express();
 
@@ -10,8 +10,10 @@ router.put("/:id" ,validateAdmin, updateHotel)
 //Delete
 router.delete("/:id" ,validateAdmin, deleteHotel)
 //Get
-router.get("/:id" , getHotel)
+router.get("/find/:id" , getHotel)
 //Get All
 router.get("/", getAllHotel)
 
+router.get("/countByCity", countByCity)
+router.get("/countByType", getAllHotel)
 export default router;
